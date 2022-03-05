0fe0b2f2a409f9475cebf3f1f9ab6011aadeff04
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-05 13:09:53 +0300

    Created login form and it's view function

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
+from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Enter your preferred username', validators=[Required()])
     email = StringField("Enter your preferred email address", validators=[Required(), Email()])
+    username = StringField('Enter your preferred username', validators=[Required()])
     password= PasswordField('Password', validators=[Required(), EqualTo('password_config', message='{asswords must match')])
     password_confirm=PasswordField('Config Password', validators=[Required()])
     submit = SubmitField('Sign UP')
+
+    #custom validators
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('An account with this email already exists')
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('The username is taken')
+
+
+class LoginForm(FlaskForm):
+    email=StringField('Your Email Address', validators=[Required(), Email()])
+    password= PasswordField('Password', validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit= SubmitField('Sign in')
