635245de3cb56408f588eea293025bd49b520c52
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-06 23:19:34 +0300

    Created pitch class and search methods

--- app/models.py
+++ app/models.py
@@ -36,10 +36,10 @@ class Comment(db.Model):
     __tablename__='comments'
 
     id= db.Column(db.Integer, primary_key=True)
-    pitch_id=db.Column(db.Integer)
     pitch_comment=db.Column(db.String())
     posted=db.Column(db.DateTime, default = datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id= db.Column(db.Integer, db.ForeignKey('pitches.id'))
     
     def __repr__(self):
         return f'User {self.pitch_comment}'
@@ -52,3 +52,31 @@ class Comment(db.Model):
     def get_comments(cls, id):
         comments=Comment.query.filter_by(pitch_id=id).all()
         return comments
+
+
+class Pitch(db.Model):
+
+    __tablename__= 'pitches'
+
+    id=db.Column(db.Integer, primary_key=True)
+    pitch_category=db.Column(db.String(255))
+    pitch_upvote = db.Column(db.Integer)
+    pitch_downvote= db.Column(db.Integer)
+    pitch_body=db.Column(db.String(300))
+    review=db.relationship('Comment', backref='pitch', lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit
+    
+    @classmethod
+    def get_pitch(cls, pitch_category):
+        pitch=Pitch.query.filter_by(pitch_category=pitch_category).all()
+        return pitch
+    @classmethod
+    def get_pitch_by_id(cls, pitch_id):
+        pitch=Pitch.query.filter_by(pitch_id=id).all()
+        return pitch
+    
