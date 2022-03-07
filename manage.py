4f5f9be84fe5767c720055c463842d66700cd722
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-08 02:56:45 +0300

    Created the command for running unittests

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User, Comment
+from app.models import User, Comment, Pitch
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -11,11 +11,19 @@ manager.add_command('server', Server)
 
 migrate=Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """Running the Unittests"""
+    import unittest
+    tests=unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 #creating shell context
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Comment=Comment)
+    return dict(app=app, db=db, User=User, Comment=Comment, Pitch=Pitch)
 
 
 if __name__ == '__main__':
